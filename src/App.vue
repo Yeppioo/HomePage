cbd192a4009c12a6a1b14c994cecd8c5bf9d673c
Yeppioo <yeppioo@qq.com>
2025-11-02 09:15:42 +0800

    refactor(css): 移除旧版CSS样式文件
    - 删除了 me.css 文件中的所有样式规则
    - 删除了 style.css 文件中的全局样式和字体定义
    - 删除了 title.css 文件中的标题相关样式
    - 移除了对已删除资源的引用和依赖

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="background-blur-overlay"></div>
+  <main>
+    <MainView />
+  </main>
+
+  <div class="tc">
+        <div onclick="" class="tc-main">
+            <img class="tc-img" alt="tc-img" />
+        </div>
+    </div>
+
+    <div class="cursor-dot" hidden></div>
+</template>
+
+<script setup lang="ts">
+import MainView from './MainView.vue';
+
+
+</script>
+
+
+<style scoped>
+.background-blur-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  backdrop-filter: blur(50px); /* 添加高斯模糊 */
+  -webkit-backdrop-filter: blur(50px); /* 兼容性 */
+  z-index: -1; /* 确保它在内容之下，但在 body 背景之上 */
+}
+body {
+  font-family: 'a', sans-serif;
+  background: url(../assets/wall.webp) no-repeat center center fixed;
+  background-size: cover;
+  color: #e0e0e0;
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  overflow: hidden; /* Prevent scrollbar during animation */
+  overflow-x: hidden;
+  width: 100vw;
+  overflow-y: scroll;
+  overflow-y: scroll;
+}
+</style>
